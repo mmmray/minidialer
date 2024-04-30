0d460bbc87b6b5c06814b06bae9cee722e9fdad4
mmmray <142015632+mmmray@users.noreply.github.com>
2024-04-30 12:49:19 +0200

    hardcode tcp-nodelay

--- src/tcp_fragment.rs
+++ src/tcp_fragment.rs
@@ -27,7 +27,7 @@ pub async fn main(args: TcpFragmentCli) {
                 }
             };
 
-            upstream.set_nodelay(args.tcp_nodelay).unwrap();
+            upstream.set_nodelay(true).unwrap();
 
             if let Err(e) = process_connection(
                 socket,
